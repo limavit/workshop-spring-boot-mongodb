4cbeb5d077a239dca92e138f0329a3e611086179
limavit <limavit@gmail.com>
2023-01-13 23:31:29 -0300

    metodo post

--- src/main/java/com/vitor/workshopmongo/services/UserService.java
+++ src/main/java/com/vitor/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vitor.workshopmongo.domain.User;
+import com.vitor.workshopmongo.dto.UserDTO;
 import com.vitor.workshopmongo.repository.UserRepository;
 import com.vitor.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -20,5 +21,12 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+		
+	}
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
