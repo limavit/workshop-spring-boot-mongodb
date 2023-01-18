35ad6ebd2ee90263fa3b722801f592d230e2a334
limavit <limavit@gmail.com>
2023-01-18 15:11:52 -0300

    acrescentando comentarios aos posts

--- src/main/java/com/vitor/workshopmongo/domain/Post.java
+++ src/main/java/com/vitor/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.vitor.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.vitor.workshopmongo.dto.AuthorDTO;
+import com.vitor.workshopmongo.dto.CommentDTO;
 @Document
 public class Post implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -17,6 +20,7 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -74,7 +78,14 @@ public class Post implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
-	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -88,6 +99,7 @@ public class Post implements Serializable{
 		return Objects.equals(id, other.id);
 	}
 
+	
 	
 
 }
